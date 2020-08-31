04b5175222a87259df43b908d6f6ab5fa290df79
Michael200217 <69688194+Michael200217@users.noreply.github.com>
2020-08-31 16:28:10 -0500

    a la de dios

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,14 @@
+package taller3.televisores;
+public class Marca {
+	String nombre;
+	public Marca(String name){
+		nombre = name;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,79 @@
+package taller3.televisores;
+
+public class TV {
+	private Marca marca;
+	private int canal=1;
+	private int precio=500;
+	private boolean estado;
+	private int volumen=1;
+	private Control control;
+	static int numTv=0;
+	public TV(Marca marc, boolean estad) {
+		marca=marc;
+		estado=estad;
+		numTv++;
+	}
+	public Marca getMarca() {
+		return marca;
+	}
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+	public Control getControl() {
+		return control;
+	}
+	public void setControl(Control control) {
+		this.control = control;
+	}
+	public int getPrecio() {
+		return precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	public int getVolumen() {
+		return volumen;
+	}
+	public void setVolumen(int volumen) {
+		this.volumen = volumen;
+	}
+	public int getCanal() {
+		return canal;
+	}
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}
+	public static int getNumTv() {
+		return numTv;
+	}
+	public void turnOn() {
+		this.estado=true;
+	}
+	public void turnOff() {
+		this.estado=false;
+	}
+	public boolean getEstado() {
+		return estado;
+	}
+	public void canalUp() {
+		if((this.estado==true)&&(this.canal<120)) {
+			this.canal++;
+		}
+	}
+	public void canalDown() {
+		if((this.estado==true)&&(this.canal>1)) {
+			this.canal--;
+		}
+	}
+	public void volumenUp() {
+		if((this.estado==true)&&(this.volumen<7)) {
+			this.volumen++;
+		}
+	}
+	public void volumenDown() {
+		if((this.estado==true)&&(this.volumen>0)) {
+			this.volumen--;
+		}
+	}
+
+}
